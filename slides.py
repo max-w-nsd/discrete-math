f3b1013b129df18cff884022afebb79768b53225
max-w-nsd <1038161@apps.nsd.org>
2025-05-08 11:15:38 -0700

    ugh how do I add the question mark

--- slides.py
+++ slides.py
@@ -19,8 +19,10 @@ class Introduction(Slide):
         text_2_1 = Text("What is")
         text_2_2 = Text("Discrete Mathematics?", font_size=72)
 
+        text_2_1.align_to(text_1_2, UP)
+
         # self.play(FadeOut(text_1_2, shift=DOWN), VGroup(text_1_1, text_2_1).animate.arrange(UP))
-        self.play(FadeOut(text_1_2, shift=DOWN), text_1_1.animate.shift(DOWN * 0.5))
+        self.play(FadeOut(text_1_2, shift=DOWN), text_1_1.animate.shift(DOWN))
         text_2_2.move_to(text_1_1)
         self.play(Write(text_2_1))
         self.play(Transform(text_1_1, text_2_2))
