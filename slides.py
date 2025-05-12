32121a15da19de4793a5439aa1ec2e7a19edaf75
max-w-nsd <1038161@apps.nsd.org>
2025-05-12 10:48:21 -0700

    ijfdiosjfo

--- slides.py
+++ slides.py
@@ -46,39 +46,44 @@ class Lesson1(Slide):
         self.play(FadeIn(apples_2, lag_ratio=0.1))
         self.play(Write(text_3_2))
 
-        self.next_slide()
+        # ADDITIVE PRINCIPLE
+        # self.next_slide()
         
-        text_4_1, text_4_2, text_4_3 = VGroup(
-            Text("14 donuts"),
-            Text("16 hotdogs"),
-            Text("How many options are there?"),
-        ).arrange(DOWN)
+        # text_4_1, text_4_2, text_4_3 = VGroup(
+        #     Text("14 donuts"),
+        #     Text("16 hotdogs"),
+        #     Text("How many options are there?"),
+        # ).arrange(DOWN)
+
+        # self.wipe([text_3_1, text_3_2, apples_2])
+        # self.play(Write(text_4_1), Write(text_4_2), Write(text_4_3))
+
+        # self.next_slide()
+
+        # text_5_1 = MathTex("14+16=30", font_size=72)
+        # text_5_1.next_to(text_4_2, DOWN)
+
+        # self.play(ReplacementTransform(text_4_3, text_5_1))
+
+        # self.next_slide()
+
+        # group_6 = VGroup(
+        #     Text("Additive principle"),
+        #     MathTex(r"A \text{or} B = A + B", font_size=72),
+        # ).arrange(DOWN)
+
+        # self.wipe([text_4_1, text_4_2, text_5_1], group_6)
+
+        self.next_slide()
+
+        lines = VGroup()
+        for i in range(-2, 3):
+            lines.add(i * RIGHT + 2 * UP, i * RIGHT + 2 * DOWN)
+        for i in range(-2, 3):
+            lines.add(i * DOWN + 2 * LEFT, i * DOWN + 2 * RIGHT)
 
         self.wipe([text_3_1, text_3_2, apples_2])
-        self.play(Write(text_4_1), Write(text_4_2), Write(text_4_3))
-
-        self.next_slide()
-
-        text_5_1 = MathTex("14+16=30", font_size=72)
-        text_5_1.next_to(text_4_2, DOWN)
-
-        self.play(Transform(text_4_3, text_5_1))
-
-        self.next_slide()
-
-        group_6 = VGroup(
-            Text("Additive principle"),
-            MathTex("A or B = A + B", font_size=72),
-        ).arrange(DOWN)
-
-        self.wipe([text_4_1, text_4_2, text_5_1], group_6)
-
-
-
-
-        
-
-
+        self.play(Create(lines, lag_ratio=0.1))
 
 
 
